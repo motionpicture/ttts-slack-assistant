5a26b48689c02ae891a6a0812ebd6ab306022059
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-07-02 03:27:46 +0000

    チャンネルへのメッセージ投稿に返答するボットを作成。

--- src/handler/event.ts
+++ src/handler/event.ts
@@ -6,15 +6,17 @@ const PRIMARY_CHANNEL = <string>process.env.PRIMARY_CHANNEL;
 const SECONDARY_CHANNEL = <string>process.env.SECONDARY_CHANNEL;
 const slackWeb = new WebClient(ACCESS_TOKEN);
 
-const HOSHIDA_SAYINGS_COLLECTION = [
-    'あー、それでいいますと、',
-    'いや、ライザップいかねえから',
-    'はい、ほしだですー',
-    'おい、否定しろよ',
-    'スターじゃねえから',
-    'からの～',
-    '粘膜接触はだめなんで',
-    '丁寧♪丁寧♪丁寧〜♪',
+const PRIMARY_STAFF_SAYINGS_COLLECTION = [
+    `ご迷惑おかけして申し訳ありません。\n状況の把握を行い原因の調査と対策を検討いたします。`,
+    `ご迷惑おかけしており大変申し訳ありません。\nただちに調査と対策の検討を行います。\n状況把握のため質問をさせていただく場合がございます。`
+    // 'あー、それでいいますと、',
+    // 'いや、ライザップいかねえから',
+    // 'はい、ほしだですー',
+    // 'おい、否定しろよ',
+    // 'スターじゃねえから',
+    // 'からの～',
+    // '粘膜接触はだめなんで',
+    // '丁寧♪丁寧♪丁寧〜♪',
 ];
 
 // Post message to Slack - https://api.slack.com/methods/chat.postMessage
@@ -29,7 +31,7 @@ export default async (event: any, callback: lambda.Callback) => {
                 // 一時問い合わせを受け付けるチャンネルにはメッセージ応答
                 if (event.channel === PRIMARY_CHANNEL) {
                     // const text = `<@${event.user}> isn't AWS Lambda awesome?`;
-                    const text = `<@${event.user}> ${HOSHIDA_SAYINGS_COLLECTION[Math.floor(Math.random() * HOSHIDA_SAYINGS_COLLECTION.length)]}`;
+                    const text = `<@${event.user}> ${PRIMARY_STAFF_SAYINGS_COLLECTION[Math.floor(Math.random() * PRIMARY_STAFF_SAYINGS_COLLECTION.length)]}`;
                     await slackWeb.chat.postMessage({
                         channel: event.channel,
                         text: text
